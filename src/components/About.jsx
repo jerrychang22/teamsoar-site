f50bdbb214e1712570dc2a895bf4b974b28ceead
RyanRosenfeld1999 <rrosenfe@stevens.edu>
2021-04-20 17:45:33 -0400

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ function About(props) {
             <AboutContainer>            
                 <Photo src={JCPNG}/>
                 <InfoBlock>
-                    <p> My name is <b>Jerry Chang</b> </p>
+                    <p> My name is <b>JerEEEEEEEEEEEEry Chang</b> </p>
                     <p> I'm a rising senior obtaining a mechanical engineering degree. I was raised in New York City and in high school I joined the school's FIRST Robotics team and would since be in love with the world of robotics. </p>
                     <p> I believe in understanding the fundamentals and building quality. Whether it's the design, the circuitry, or the programming, I like building the project from the ground up and learning as much as I can. Extensibility is very important to me. Making something easier to develop, maintain, and upgrade creates long-lasting and less headache-inducing products.</p>
                 </InfoBlock>
